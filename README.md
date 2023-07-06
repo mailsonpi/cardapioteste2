aa16612d90ba4d5e45212cce4a20e078dcc475c2
MailsonAlmeidaCerqueira <110990041+mailsonpi@users.noreply.github.com>
2023-07-06 11:08:40 -0300

    commit init

---
+++ README.md
@@ -0,0 +1,5 @@
+# CARDAPIO DIGITAL JAVASCRIPT, JQUERY E JSON
+### cardapiodigital responsivo com integração ao whatsapp!
+Acesse a aplicação em: https://gabrielfranca95.github.io/cardapiodigital/
+![image](https://user-images.githubusercontent.com/57453192/159144643-9c698ca3-65c4-4b0e-bd29-9e643198e3ba.png)
+# CardapioFinal
---
+++ bebidas.js
@@ -0,0 +1,17 @@
+let menuJson = [
+    {id:1, name:'REFRIGERANTE GUARANA R$ 9,90', img:'guarana.jpg', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'pão com gergelim, 2 carnes, alface, tomate e cebola e molho especial'},
+    {id:2, name:'COCA COLA R$ 9,90', img:'cocacola.jfif', price:[9.90, 9.90, 9.90], sizes:[' ', '',''], description:'pão com gergelim, carne grelhada, maionese especial, alface, tomate, cebola picles'},
+    {id:3, name:'HEINEKEN R$ 12,00', img:'heineken2.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'3 Carnes, queijo 3 vezes, pão, molho especial, picles, tomate e cebola '},
+    {id:4, name:'SKOL R$ 8,00', img:'skol.png', price:[8.00, 8.00, 8.00], sizes:[' ', '',''], description:'pão com gergelim, queijo em dobro, tomate, cebola, alface, picles '},
+    {id:5, name:'STELLA R$ 8,90', img:'image/cheddarduplo.jpeg', price:[8.90, 8.90, 8.90], sizes:[' ', '',''], description:'pão com gergelim, 2 caenes, cheddar em dobro, cebola caramelizada'},
+    {id:6, name:'AGUA COM GAS R$ 11,80', img:'image/hfrango.jpeg', price:[11.80, 11.80, 11.80], sizes:[' ', '',''], description:'pão com gergelim, molho especial, chicken empanado, alface, tomate, cebola '},
+    {id:7, name:'DRIK DE UVAS R$ 18,90', img:'image/catupiry.jpg', price:[18.90, 18.90, 18.90], sizes:[' ', '',''], description:'pão com gergelim, 4 carnes de hamburguer, catupiry, bacon e molho especial'},
+    {id:8, name:'PIZZA DE PEPERONE R$ 38,00', img:'image/novapizza.jpg', price:[38.00, 38.00, 38.00], sizes:['','', ''], description:'Pizza de peperone: massa a base de trigo, molho de tomate, queijo mussarela, deliciosas fatias de peperone'},
+    {id:9, name:'AGUA MINERAL R$2,00', img:'image/agua.jpeg', price:[2.00, 2.00, 2.00], sizes:['','', ''], description:'agua mineral 500ml'},
+    {id:10, name:'HAMBURGUER R$ 7,90 ', img:'image/xburguercomum.jpg', price:[7.90, 7.90, 7.90], sizes:[' ', '',''], description:'pão, 1 carne de hamburguer, queijo, picles, tomate, cebola e molho especial'},
+    {id:11, name:'ONIONS BURGUER R$ 10,90', img:'32.png', price:[10.90, 10.90, 10.90], sizes:[' ', '',''], description:'pão, 2 carnes, onions empanadas, queijo , bacon'},
+    {id:12, name:'TRIPLO4444444444 BURGUER R$ 12,00', img:'32.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'3 Carnes, queijo 3 vezes, pão, molho especial, picles, tomate e cebola '},
+     {id:13, name:'TRIPLO2 BURGUER R$ 12,00', img:'32.png', price:[12.00, 12.00, 12.00], sizes:[' ', '',''], description:'3 Carnes, queijo 3 vezes, pão, molho especial, picles, tomate e cebola '},
+ 
+   
+];
